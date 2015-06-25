05e2cbc812d1daaece11210ad2a7f3ee40a4f797
Gabriel Renom <gabrielrenom@gmail.com>
2015-06-25 22:23:14 +0100

    Extras added

--- ACP.Business.Test/BookingEntityTest.cs
+++ ACP.Business.Test/BookingEntityTest.cs
@@ -9,6 +9,7 @@ using ACP.DataAccess.Managers;
 using ACP.Business.Models;
 using System.Collections;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ACP.Business.Test
 {
@@ -98,14 +99,85 @@ namespace ACP.Business.Test
 
         }
 
+        [TestMethod]
+        public void GivenAEntityWithExtrasAndPrices_WhenIsAdded_BeSureItRestunsTrueAndRemoves()
+        {
+            //Arrange
+            BookingEntityModel model = new BookingEntityModel();
+            model.Comission = 2;
+            model.Created = DateTime.Now;
+            model.CreatedBy = localuser;
+            model.Modified = DateTime.Now;
+            model.ModifiedBy = localuser;
+            model.Name = "Local Airport" + DateTime.Now.ToString();
+            model.RootBookEntityId = 1;
+            model.Address = new AddressModel
+            {
+                Address1 = "MyAdress",
+                Postcode = "SK74QW",
+                Country = "UK"
+            };
+            model.Extras = new Collection<ExtraModel>();
+            model.Extras.Add(new ExtraModel
+            {
+                Created = DateTime.Now,
+                CreatedBy = localuser,
+                Modified = DateTime.Now,
+                ModifiedBy = localuser,
+                Name = "Car Wash",
+                Price = 10,
+                Description = "Car Wash"
+
+            });
+            model.Prices = new Collection<BookingPricingModel>();
+            model.Prices.Add(new BookingPricingModel
+            {
+                Created = DateTime.Now,
+                CreatedBy = localuser,
+                Modified = DateTime.Now,
+                Name = "Winter",
+                Start = DateTime.Now,
+                End = DateTime.Now,
+                DayPrices = new Collection<DayPriceModel>{
+                    new DayPriceModel
+                    {
+                        Created = DateTime.Now,
+                        Day = 1,
+                        Modified = DateTime.Now,
+                        Dayprice = 660,
+                        CreatedBy = localuser,
+                        HourPrices = new Collection<HourPriceModel>() 
+                        { 
+                            new HourPriceModel
+                            {
+                                 Created = DateTime.Now,                         
+                                 HourMinute = DateTime.Now,
+                                 Hourprice = 666,
+                                  Modified= DateTime.Now
+                            }
+                        }
+                    }                    
+                }
+            });
+        
+
+            //Act
+            var results = service.Add(model);
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.IsTrue(results);
+
+
+        }
 
         [TestMethod]
         public void GivenAEntity_WhenIsUpadted_BeSureItReturnsUpdatedData()
         {
-            var bookingentities = service.GetAllBookingEntities()[0];
+            var bookingentities = service.GetAllBookingEntities()[7];
             bookingentities.Price = 999;
             bookingentities.Address.Postcode = "SK74QW";
-
+            bookingentities.Prices.FirstOrDefault().DayPrices.FirstOrDefault().HourPrices.FirstOrDefault().Hourprice = 8888;
             //Act
             var results = service.Update(bookingentities);
 
@@ -116,7 +188,7 @@ namespace ACP.Business.Test
         [TestMethod]
         public void GivenAEntity_WhenIsDeleted_BeSureItIsNotThere()
         {
-            var bookingentities = service.GetAllBookingEntities()[0];          
+            var bookingentities = service.GetAllBookingEntities()[7];          
 
             //Act
             var results = service.Remove(bookingentities.Id);
