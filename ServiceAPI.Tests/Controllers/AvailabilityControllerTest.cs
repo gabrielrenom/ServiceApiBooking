78bb7c57c79558f266c1bb11d9de6f15f34889a6
Gabriel Renom <gabrielrenom@gmail.com>
2015-09-16 08:04:16 +0100

    Availability api test started.

---
+++ ServiceAPI.Tests/Controllers/AvailabilityControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using ACP.Business.Managers;
+using ACP.Business.Repository;
+using ACP.Business.Services;
+using ACP.Business.Services.Interfaces;
+using ACP.DataAccess.Managers;
+using ACP.DataAccess.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAPI.Tests.Controllers
+{
+    [TestClass]
+    public class AvailabilityControllerTest
+    {
+        private AvailabilityController availabilitycontroller;
+        private IAvailabilityService service;
+        private IAvailabilityManager manager;
+        private IACPRepository repository;
+
+        [TestMethod]
+        public void Setup()
+        {
+            repository = new ACPRepository();
+            manager = new AvailabilityManager(repository);
+            service = new AvailabilityService(manager);
+            availabilitycontroller = new AvailabilityController(service);
+        }
+    }
+}
