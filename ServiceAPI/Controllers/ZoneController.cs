4be9771bdb84b3153bc5d4c4745e0ba58afb137a
Gabriel Renom <gabrielrenom@gmail.com>
2015-09-23 07:41:42 +0100

    Zone controller 90% finish

---
+++ ServiceAPI/Controllers/ZoneController.cs
@@ -0,0 +1,475 @@
+﻿using ACP.Business;
+using ACP.Business.Enums;
+using ACP.Business.Exceptions;
+using ACP.Business.Models;
+using ACP.Business.Services.Interfaces;
+using ServiceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Security;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ServiceAPI.Controllers
+{
+    [RoutePrefix("api/v0.1/zonecontroller")]
+    public class ZoneController : BaseApiController
+    {
+        private IZoneService _zoneservice;
+
+        public ZoneController(IZoneService zoneservice)
+        {
+            _zoneservice = zoneservice;
+        }
+
+        [HttpGet]
+        [Route("getbyid")]
+        public async Task<HttpResponseMessage> GetById(int id)
+        {
+            ZoneModel zone = null;
+            try
+            {
+                zone = await _zoneservice.GetById(id);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zone, new JsonMediaTypeFormatter());
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public async Task<HttpResponseMessage> GetAll()
+        {
+            IList<ZoneModel> zones = null;
+            try
+            {
+                zones = await _zoneservice.GetAll();
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zones, new JsonMediaTypeFormatter());
+        }
+
+        [HttpGet]
+        [Route("getbyname")]
+        public async Task<HttpResponseMessage> GetByName(int? number=null,string identifier=null)
+        {
+            ZoneModel zone = null;
+            try
+            {
+                zone = await _zoneservice.GetByNumberIdentifier(number,identifier);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zone, new JsonMediaTypeFormatter());
+        }
+
+        [HttpGet]
+        [Route("getallfree")]
+        public async Task<HttpResponseMessage> GetAllFree()
+        {
+            IList<ZoneModel> zones = null;
+            try
+            {
+                zones = await _zoneservice.GetAllFree();
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zones, new JsonMediaTypeFormatter());
+        }
+
+        [HttpGet]
+        [Route("getalloccupied")]
+        public async Task<HttpResponseMessage> GetAllOccupied()
+        {
+            IList<ZoneModel> zones = null;
+            try
+            {
+                zones = await _zoneservice.GetAllOccupied();
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zones, new JsonMediaTypeFormatter());
+        }
+
+        [HttpGet]
+        [Route("findzoneavailable")]
+        public async Task<HttpResponseMessage> FindZoneAvailable(DateTime startdate, DateTime enddate, string airport)
+        {
+            IList<ZoneModel> zones = null;
+            try
+            {
+                zones = await _zoneservice.FindZoneAvailable(startdate, enddate);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zones, new JsonMediaTypeFormatter());
+        }
+
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<HttpResponseMessage> Add(ZoneModel model)
+        {
+            ZoneModel zone = null;
+            try
+            {
+                zone = await _zoneservice.Add(model);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zone, new JsonMediaTypeFormatter());
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<HttpResponseMessage> Update(ZoneModel model)
+        {
+            bool zone = false;
+            try
+            {
+                zone = await _zoneservice.Update(model);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zone, new JsonMediaTypeFormatter());
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<HttpResponseMessage> Delete(int id)
+        {
+            bool zone = false;
+            try
+            {
+                zone = await _zoneservice.Remove(id);
+            }
+            catch (HttpRequestException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            catch (SecurityException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.Forbidden, ex.Message);
+            }
+            catch (ItemNotFoundException ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex.Message);
+            }
+            catch (ValidationErrorsException ex)
+            {
+                var errorMessages = ex.ValidationErrors.Select(x => x.ErrorMessage);
+
+                var exceptionMessage = string.Concat("The request is invalid: ", string.Join("; ", errorMessages));
+
+                Trace.TraceError(exceptionMessage);
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exceptionMessage);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, zone, new JsonMediaTypeFormatter());
+        }
+
+
+        private RootBookingPropertyViewModel ToViewModel(RootBookingEntityModel dataModel)
+        {
+            RootBookingPropertyViewModel viewmodel = new RootBookingPropertyViewModel
+            {
+                Id = dataModel.Id,
+                Name = dataModel.Name,
+                Address = new AddressViewModel
+                {
+                    Id = dataModel.Address.Id,
+                    Address1 = dataModel.Address.Address1,
+                    Address2 = dataModel.Address.Address2,
+                    Country = dataModel.Address.Country,
+                    County = dataModel.Address.County,
+                    Number = dataModel.Address.Number,
+                    Postcode = dataModel.Address.Postcode
+                },
+                AddressId = dataModel.AddressId,
+                StatusId = dataModel.StatusId,
+                Telephone = dataModel.Telephone,
+                Status = new StatusViewModel
+                {
+                    Id = dataModel.Status.Id,
+                    StatusType = (Enums.StatusType)dataModel.Status.StatusType
+                }
+            };
+
+            return viewmodel;
+        }
+
+        private RootBookingEntityModel ToDataModel(RootBookingPropertyViewModel dataModel)
+        {
+            RootBookingEntityModel model = new RootBookingEntityModel();
+            model.Id = dataModel.Id;
+            model.Name = dataModel.Name;
+            model.Address = new AddressModel
+            {
+                Id = dataModel.Address.Id,
+                Address1 = dataModel.Address.Address1,
+                Address2 = dataModel.Address.Address2,
+                Country = dataModel.Address.Country,
+                County = dataModel.Address.County,
+                Number = dataModel.Address.Number,
+                Postcode = dataModel.Address.Postcode
+            };
+
+            model.AddressId = dataModel.AddressId;
+            model.StatusId = dataModel.StatusId;
+            model.Telephone = dataModel.Telephone;
+            model.Status = new StatusModel
+            {
+                Id = dataModel.Status.Id,
+                StatusType = (StatusType)dataModel.Status.StatusType
+            };
+            if (dataModel.Id == 0)
+            {
+                model.Created = DateTime.Now;
+                model.CreatedBy = "local";
+            }
+
+            model.Modified = DateTime.Now;
+            model.ModifiedBy = "local";
+
+            return model;
+        }
+    }
+}
