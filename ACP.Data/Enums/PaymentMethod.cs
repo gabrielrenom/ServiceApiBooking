e1b76df5783368380437ae80e655eeb75b0a7b40
Gabriel Renom <gabrielrenom@gmail.com>
2016-01-29 13:06:04 +0000

    Portal Added on ASP.NET 5

---
+++ ACP.Data/Enums/PaymentMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACP.Data.Enums
+{
+    public enum PaymentMethod
+    {
+        CreditCard = 100,
+        BankAccount = 101,
+        PayPal = 102,
+        DebitCard = 103
+    }
+}
