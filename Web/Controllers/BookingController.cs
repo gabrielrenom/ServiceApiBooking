bef57ddc828b4ae79cf5e1b2888f4b2d781d01a6
Gabriel Renom <gabrielrenom@gmail.com>
2016-05-25 22:55:21 +0100

    Added Validation of Booking

--- Web/Controllers/BookingController.cs
+++ Web/Controllers/BookingController.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class BookingController : Controller
     {
         // GET: Booking
+        [HttpGet]
         public ActionResult Index()
         {
+
+            BookingGuestViewModel model = new BookingGuestViewModel();
+
+            return View(model);
+        }
+
+        // GET: Booking
+        [HttpPost]
+        public ActionResult Index(BookingGuestViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+              
+            }
+
             return View();
         }
     }
---
+++ Web/Models/BookingGuestViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class BookingGuestViewModel
+    {
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ConfirmEmail { get; set; }
+
+        [Required()]
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required]
+        public string Mobile { get; set; }
+
+        [Range(1, 8)]
+        [Required]
+        public int Passangers { get; set; }
+
+        [Required]
+        public string Registration { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+        public string TerminalIn { get; set; }
+
+        [Required]
+        public string OutboundFlight { get; set; }
+        public string TerminalOut { get; set; }
+
+        [Required]
+        public string InboundFlight { get; set; }
+        public string CouponDiscount { get; set; }
+        public bool IsAddSMSConfirmation { get; set; }
+        public bool IsCancelationCover { get; set; }
+        public bool IsAddCarWashService { get; set; }
+
+    }
+}
