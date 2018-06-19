44e0e6a39c90000a5669b6851c456decaf73e4fb
Gabriel Renom <gabriel.renom@GTS.local>
2018-06-19 15:27:12 +0100

    Dates fixed in the portal

---
+++ ACP.Commons/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace ACP.Commons
+{
+    public class DateHelper
+    {
+        public static DateTime ConvertToUKDateTime(string date) => DateTime.ParseExact(date, "dd-MM-yyyy HH:mm:ss", new CultureInfo("en-UK"));
+
+        public static DateTime ConvertToUKDateTime(DateTime date) => Convert.ToDateTime(date, new CultureInfo("en-UK"));
+    }
+}
